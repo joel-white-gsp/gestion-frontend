46012ebe23503ae682d43cda1ac06053075a5c2a
Noman Ahmed <nomanahmeda789@gmail.com>
2024-07-23 08:18:25 -0700

    feat: work on inital ui

--- src/views/home/PageContent.jsx
+++ src/views/home/PageContent.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-
+import HomeBanner from "./section/HomeBanner";
+import Gym from "./section/Gym";
+import HomeCarousel from "./section/HomeCarousel";
+import HomeCard from "./section/HomeCard";
+import LocateGym from "./section/LocateGym";
 
 const PageContent = () => {
-    return (
-        <Wrapper>
-            <div className="comming-soon-text">
-                Home
-            </div>
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <div className="home-container">
+        <HomeBanner />
+        <Gym />
+        <HomeCarousel />
+        <LocateGym />
+      </div>
+    </Wrapper>
+  );
+};
 
+export default PageContent;
 
-export default PageContent
-
-const Wrapper = styled.div`
-    background-color:#fff;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .comming-soon-text{
-        font-size: 40px;
-        font-weight: bold;
-    }
-`
+const Wrapper = styled.div``;
